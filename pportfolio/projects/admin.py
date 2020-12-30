457bf3121495c93309af1a118b8eff68b021f3fc
paulpbolton <paul.bolton2@gmail.com>
2020-12-30 10:02:54 +0200

    first commit webpage

---
+++ pportfolio/projects/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from projects.models import Project_inv,Project_inv2,Project_inv3
+# Register your models here.
+admin.site.register(Project_inv)
+admin.site.register(Project_inv2)
+admin.site.register(Project_inv3)
+
